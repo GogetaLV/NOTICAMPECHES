2b4b3f0a7c78d00ac04e199f816cdddc2a747947
GogetaLV <luisvazquez0800@gmail.com>
2023-10-29 16:28:12 -0600

    Pantalla de mnu

--- app/src/main/java/com/example/notiapp/MainActivity.kt
+++ app/src/main/java/com/example/notiapp/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Button
 import com.example.notiapp.R
 
 class MainActivity : AppCompatActivity() {
@@ -15,10 +16,18 @@ class MainActivity : AppCompatActivity() {
         tvGoRegister.setOnClickListener{
             goToRegister()
         }
+        val btnGoMenu = findViewById<Button>(R.id.tv_go_to_menu)
+        btnGoMenu.setOnClickListener{
+
+        }
 
     }
     private fun goToRegister() {
         val i = Intent(this, RegisterActivity:: class.java)
+        startActivity(i)
+    }
+    private fun btnGoMenu (){
+        val i = Intent(this, MenuActivity::class.java)
         startActivity(i)
     }
 }
