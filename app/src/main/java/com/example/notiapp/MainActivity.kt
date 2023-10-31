3356c0900736d810970362c1ca81223318f25fba
GogetaLV <luisvazquez0800@gmail.com>
2023-10-31 17:58:11 -0600

    Persistencia de datos con Shared Preferences

--- app/src/main/java/com/example/notiapp/MainActivity.kt
+++ app/src/main/java/com/example/notiapp/MainActivity.kt
@@ -5,12 +5,20 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Button
+import com.example.notiapp.util.PreferenceHelper
+import com.example.notiapp.util.PreferenceHelper.get
+import com.example.notiapp.util.PreferenceHelper.set
 import com.example.notiapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val preferences = PreferenceHelper.defaultPrefs(this)
+        if (preferences["session", false])
+            goToMenu()
+
 
         val tvGoRegister = findViewById<TextView>(R.id.tv_go_to_register)
         tvGoRegister.setOnClickListener{
@@ -28,6 +36,12 @@ class MainActivity : AppCompatActivity() {
     }
     private fun btnGoMenu (){
         val i = Intent(this, MenuActivity::class.java)
+        createSessionPreferences()
         startActivity(i)
+        finish()
+    }
+    private fun createSessionPreferences(){
+        val preferences = PreferenceHelper.defaultPrefs(this)
+        preferences["session"] = true
     }
 }
--- app/src/main/java/com/example/notiapp/MenuActivity.kt
+++ app/src/main/java/com/example/notiapp/MenuActivity.kt
@@ -1,11 +1,32 @@
 package com.example.notiapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.notiapp.util.PreferenceHelper
+import com.example.notiapp.util.PreferenceHelper.set
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        val btnLogout = findViewById<Button>(R.id.btn_logout)
+        btnLogout.setOnClickListener {
+            clearSessionPreferences()
+            btnLogin()
+        }
+    }
+
+    private fun goToLogin() {
+        val i = Intent(this, MainActivity::class.java)
+        startActivity(i)
+        finish()
+    }
+
+    private fun clearSessionPreferences() {
+        val preferences = PreferenceHelper.defaultPrefs(this)
+        preferences["session"] = false
     }
 }
