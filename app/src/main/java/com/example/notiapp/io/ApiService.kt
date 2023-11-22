d1a066ff7ffc509e6a38f30b0d4931cb3a7412e6
GogetaLV <luisvazquez0800@gmail.com>
2023-11-22 12:41:45 -0600

    Vía Api

--- app/src/main/java/com/example/notiapp/io/ApiService.kt
+++ app/src/main/java/com/example/notiapp/io/ApiService.kt
@@ -1,5 +1,14 @@
 package com.example.notiapp.io
-
+import retrofit2.htt.POST
 interface ApiService {
+@POST(value = "Login")
+fun (Query(value = "email") email:String,@Query(value="password") password:String):
+    call<LoginResponse>
+companion object{
+    private const val BASE_URL =""
+    fun create():ApiService{
+        val retrofit = Retrofit.Buildeer()
+    }
 
+}
 }
