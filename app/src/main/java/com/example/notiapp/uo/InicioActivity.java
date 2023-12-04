0e9bda52de34c92ccaabda84be7e08b73eef39c9
GogetaLV <luisvazquez0800@gmail.com>
2023-12-04 11:56:32 -0600

    Menu class funcion

---
+++ app/src/main/java/com/example/notiapp/uo/InicioActivity.java
@@ -0,0 +1,21 @@
+package com.example.notiapp.uo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class InicioActivity extends AppCompatActivity {
+DrawerLayout drawerLayout;
+NavigationView navigationView;
+ActionBarDrawerToggle drawerToggle;
+@Override
+    protected void onCreate(Bundle savedInstanceState){
+    super.onCreate(savedInstanceState);
+    
+}
+}
